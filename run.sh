d4822178768013fed8f022012d9e78e47e99a502
acanimal <asantiago@acuriousanimal.com>
2016-07-10 18:03:27 +0200

    Show drafts on development mode

--- run.sh
+++ run.sh
@@ -3,21 +3,4 @@
 #
 # Simply helper script to run jekyll in development environment
 #
-
-environment="$1"
-
-if [[ "$environment" != "dev"  &&  "$environment" != "prod" ]]
-then
-  echo "Error!!! A parameter is required."
-  echo "Usage: "
-  echo "   > $0 [dev|prod]"
-fi
-
-case $environment in
-  "dev")  echo "Running un dev mode..."
-          jekyll serve --config=_config.yml,_config-dev.yml
-          ;;
-  "prod") echo "Running in prod mode... (You should not use this in localshot !!!)"
-          jekyll serve
-          ;;
-esac
+jekyll serve --drafts --config=_config.yml,_config-dev.yml
