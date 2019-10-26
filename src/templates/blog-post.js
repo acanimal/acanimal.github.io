663399ed9ac7a38adb2fbb52c7dde0f2507cbc4f
acanimal <asantiagop@gmail.com>
2019-10-26 17:47:55 +0200

    Initial commit from gatsby: (https://github.com/niklasmtj/gatsby-starter-julia.git)

---
+++ src/templates/blog-post.js
@@ -0,0 +1,89 @@
+import React from "react"
+import { graphql } from "gatsby"
+import styled from "@emotion/styled"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Content = styled.div`
+  margin: 0 auto;
+  max-width: 860px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const MarkedHeader = styled.h1`
+  display: inline;
+  border-radius: 1em 0 1em 0;
+  background-image: linear-gradient(
+    -100deg,
+    rgba(255, 250, 150, 0.15),
+    rgba(255, 250, 150, 0.8) 100%,
+    rgba(255, 250, 150, 0.25)
+  );
+`
+
+const HeaderDate = styled.h3`
+  margin-top: 10px;
+  color: #606060;
+`
+
+// STYLE THE TAGS INSIDE THE MARKDOWN HERE
+const MarkdownContent = styled.div`
+  a {
+    text-decoration: none;
+    position: relative;
+  }
+
+  a::after {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    top: 70%;
+    left: -0.1px;
+    right: -0.1px;
+    bottom: 0;
+    transition: top 0.1s ease-in-out;
+    background-color: rgba(255, 250, 150, 0.8);
+  }
+
+  a:hover::after {
+    top: 0;
+  }
+`
+
+export default ({ data }) => {
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <Content>
+        <MarkedHeader>{post.frontmatter.title}</MarkedHeader>
+        <HeaderDate>
+          {post.frontmatter.date} - {post.fields.readingTime.text}
+        </HeaderDate>
+        <MarkdownContent dangerouslySetInnerHTML={{ __html: post.html }} />
+      </Content>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      excerpt(pruneLength: 160)
+      frontmatter {
+        date(formatString: "DD MMMM, YYYY")
+        path
+        title
+      }
+      fields {
+        readingTime {
+          text
+        }
+      }
+    }
+  }
+`
